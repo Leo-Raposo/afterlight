5c916a4359dc849b2b9110b280f95456fe2af55d
Leo-Raposo <apoena@live.com>
2024-09-24 21:43:28 -0300

    Adding Sign Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SliderComponent } from './components/home/home.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SigninComponent],
+  imports: [RouterOutlet, SignupComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
