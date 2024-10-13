a0d20184f56779a84b7271762e5a5d015f5a72fb
Filipe Almeida Simões <filipesimoes439@gmail.com>
2024-10-12 22:32:27 -0300

    Ajustando front para consumir API

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service'; // Ajuste o caminho de acordo com seu projeto
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  signupForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.signupForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    if (this.signupForm.valid) {
+      const { name, email, password } = this.signupForm.value;
+
+      try {
+        await this.authService.register({username: name, email, password });
+        this.router.navigate(['/signin']); // Redireciona para a página de login após o registro
+      } catch (error) {
+        console.error('Registration failed:', error);
+      }
+    }
+  }
 }
